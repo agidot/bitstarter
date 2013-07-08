13537bfc2316ad461169ac08e0235c2e03a870b6
Agi dot <agidot@gmail.com>
2013-07-08 17:45:42 +0700

    hw 3 part 1

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var buffer = fs.readFileSync('index.html');
+var text = buffer.toString('utf8');
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
